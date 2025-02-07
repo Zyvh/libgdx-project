9ddb3cf29ac90bcd5baf68530bd6e9c05acd0610
Zyvh <nassim59460@gmail.com>
2025-02-07 11:20:31 +0100

    Version 0.0.1

--- core/src/main/java/fr/dampierre/GameApp.java
+++ core/src/main/java/fr/dampierre/GameApp.java
@@ -4,25 +4,47 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GameApp extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private Rectangle marioBounds;
+    private boolean marioClicked;
+    private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("mario.png");
+        marioBounds = new Rectangle(100, 100, 83, 95); // Position initiale de Mario
+        marioClicked = false;
+        font = new BitmapFont(); // Police pour afficher "GAGNÉ !"
     }
 
     @Override
     public void render() {
+        Vector2 clic = null;
+        if (Gdx.input.isTouched()) {
+            clic = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+            clic.y = Gdx.graphics.getHeight() - clic.y; // Ajustement Y
+
+            if (marioBounds.contains(clic)) {
+                marioClicked = true;
+            }
+        }
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        if (marioClicked) {
+            font.draw(batch, "Gagné", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
+        } else {
+            batch.draw(image, marioBounds.x, marioBounds.y, marioBounds.getWidth(), marioBounds.getHeight());
+        }
         batch.end();
     }
 
@@ -30,5 +52,6 @@ public class GameApp extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+        font.dispose();
     }
 }
