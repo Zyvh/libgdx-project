4ad13112469428e62688473ea923987fab777c7f
Zyvh <nassim59460@gmail.com>
2025-02-07 12:13:36 +0100

    Version 0.0.2 3/4

--- core/src/main/java/fr/dampierre/GameApp.java
+++ core/src/main/java/fr/dampierre/GameApp.java
@@ -9,21 +9,48 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.MathUtils;
 
 public class GameApp extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture marioImage;
+    private Texture luigiImage;
+    private Texture warioImage;
+    private Texture yoshiImage;
     private Rectangle marioBounds;
+    private Rectangle luigiBounds;
+    private Rectangle warioBounds;
+    private Rectangle yoshiBounds;
     private boolean marioClicked;
+    private boolean luigiClicked;
+    private boolean warioClicked;
+    private boolean yoshiClicked;
+    private boolean gagne;
+    private boolean perdu;
     private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("mario.png");
-        marioBounds = new Rectangle(100, 100, 83, 95); // Position initiale de Mario
+        marioImage = new Texture("mario.png");
+        luigiImage = new Texture("luigi.png");
+        warioImage = new Texture("wario.png");
+        yoshiImage = new Texture("yoshi.png");
+        marioBounds = new Rectangle(MathUtils.random(0, Gdx.graphics.getWidth() - 83),
+                MathUtils.random(0, Gdx.graphics.getHeight() - 95), 83, 95);
+        luigiBounds = new Rectangle(MathUtils.random(0, Gdx.graphics.getWidth() - 83),
+                MathUtils.random(0, Gdx.graphics.getHeight() - 95), 83, 95);
+        warioBounds = new Rectangle(MathUtils.random(0, Gdx.graphics.getWidth() - 83),
+                MathUtils.random(0, Gdx.graphics.getHeight() - 95), 83, 95);
+        yoshiBounds = new Rectangle(MathUtils.random(0, Gdx.graphics.getWidth() - 83),
+                MathUtils.random(0, Gdx.graphics.getHeight() - 95), 83, 95);
         marioClicked = false;
-        font = new BitmapFont(); // Police pour afficher "GAGNÉ !"
+        luigiClicked = false;
+        warioClicked = false;
+        yoshiClicked = false;
+        gagne = false;
+        perdu = false;
+        font = new BitmapFont();
     }
 
     @Override
@@ -35,23 +62,58 @@ public class GameApp extends ApplicationAdapter {
 
             if (marioBounds.contains(clic)) {
                 marioClicked = true;
+            } else if (luigiBounds.contains(clic)) {
+                luigiClicked = true;
+            } else if (warioBounds.contains(clic)) {
+                warioClicked = true;
+            } else if (yoshiBounds.contains(clic)) {
+                yoshiClicked = true;
             }
         }
 
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
+
         if (marioClicked) {
-            font.draw(batch, "Gagné", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
-        } else {
-            batch.draw(image, marioBounds.x, marioBounds.y, marioBounds.getWidth(), marioBounds.getHeight());
+            font.draw(batch, "Gagné!", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
+            gagne = true;
         }
+        if (luigiClicked) {
+            font.draw(batch, "Perdu!", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
+            perdu = true;
+        }
+        if (warioClicked) {
+            font.draw(batch, "Perdu!", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
+            perdu = true;
+        }
+        if (yoshiClicked) {
+            font.draw(batch, "Perdu!", Gdx.graphics.getWidth() / 2f - 30, Gdx.graphics.getHeight() / 2f);
+            perdu = true;
+        }
+
+        if (gagne == false || perdu == false) {
+            batch.draw(marioImage, marioBounds.x, marioBounds.y, marioBounds.getWidth(), marioBounds.getHeight());
+            batch.draw(luigiImage, luigiBounds.x, luigiBounds.y, luigiBounds.getWidth(), luigiBounds.getHeight());
+            batch.draw(warioImage, warioBounds.x, warioBounds.y, warioBounds.getWidth(), warioBounds.getHeight());
+            batch.draw(yoshiImage, yoshiBounds.x, yoshiBounds.y, yoshiBounds.getWidth(), yoshiBounds.getHeight());
+        }
+
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        marioImage.dispose();
+        font.dispose();
+        batch.dispose();
+        luigiImage.dispose();
+        font.dispose();
+        batch.dispose();
+        warioImage.dispose();
+        font.dispose();
+        batch.dispose();
+        yoshiImage.dispose();
         font.dispose();
     }
 }
